81db7a5ccfead83c4e292c173edc74a460f0fb64
ACE User <aceuser@e140-s07.lab.lagcc>
2017-06-12 21:17:37 -0400

    made dropdown

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-console.log('this works');
+// console.log('this works');
+
+var topNavList = document.querySelector('#top-nav-list');
+var isCollapsed = true;
+var topNavToggle = document.querySelector('#top-nav-toggle');
+
+topNavToggle.addEventListener('click', function(){
+isCollapsed = !isCollapsed;
+if(isCollapsed === false){
+	topNavList.classList.remove('nav-list-collapse');
+	topNavToggle.innerHTML = '<i class="fa fa-times" aria-hidden="true"></i>';
+}
+else{
+	topNavList.classList.add('nav-list-collapse');
+	topNavToggle.innerHTML = '<i class="fa fa-bars" aria-hidden="true"></i>';
+}
+});
